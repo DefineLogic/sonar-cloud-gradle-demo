a132e9b4a434f41d882c6e199ba2cc62a07c61ef
kamal <comeallaalay@gmail.com>
2019-09-10 10:04:51 +0530

    first commit

---
+++ src/main/java/com/example/sonarcloudgradledemo/SonarCloudGradleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonarcloudgradledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarCloudGradleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarCloudGradleDemoApplication.class, args);
+	}
+
+}
